6911ddd9f7e21b6e088224517c760cd0aa5efb44
Jeevan Kadam <24BTCSE080@raiuniversity.edu>
2025-12-20 23:47:22 +0530

    Day 1: Implement custom Promise in JavaScript

--- JS/Day_1/custompromise.js
+++ JS/Day_1/custompromise.js
@@ -2,20 +2,34 @@
 
 
 
-function customPromise(){
-    return  new Promise((resolve,reject)=>{
-          const success = true;
-            if(success){
-                resolve("Promise Resolved Successfully");
-            }else{
-                reject("Promise Rejected");
-            }
-    });
-};
-customPromise().then(console.log).catch(console.log);
+// function customPromise(){
+//     return  new Promise((resolve,reject)=>{
+//           const success = true;
+//             if(success){
+//                 resolve("Promise Resolved Successfully");
+//             }else{
+//                 reject("Promise Rejected");
+//             }
+//     });
+// };
+// customPromise().then(console.log).catch(console.log);
 
 /// without using inbuilt function
 
 // function myPromiese(){
 
 // }
+
+
+function custompromise(){
+    return new promises((resolve,reject)=>{
+        const success = true;
+        if(success){
+            resolve("Promises Resolved Successfull");
+        }else{
+            reject("promise reject");
+        }
+    });
+};
+
+custompromise().then(console.log).catch(console.log);

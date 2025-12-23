f756ac86e0ded77eab9cb4bb7e24f89b15b728d4
Jeevan Kadam <24BTCSE080@raiuniversity.edu>
2025-12-24 00:08:24 +0530

    Improve solution for Remove Duplicates from Sorted Array

--- DSA/26. Remove Duplicates from Sorted Array/better.cpp
+++ DSA/26. Remove Duplicates from Sorted Array/better.cpp
@@ -1,21 +1,24 @@
 
-#include <iostream>
-#include <vector>
-using namespace std;
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()) return 0;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         if(nums.empty()) return 0;
 
-        int j=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[j]!=nums[i]){
-                nums[++j] = nums [i];
-            }
-        }
-        return j+1;
-    }
-};      
+//         int j=0;
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[j]!=nums[i]){
+//                 nums[++j] = nums [i];
+//             }
+//         }
+//         return j+1;
+//     }
+// };      
 
 // time complexity: O(n)
 // space complexity: O(1)
+
+// using a sorting method to solve this problem will increase time complexity to O(nlogn) which is not optimal as the array is already sorted.
+

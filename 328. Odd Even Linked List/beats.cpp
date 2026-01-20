768f0633b7f531afaeebfd0c10c2d1e9aef2eedc
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-01-21 00:22:56 +0530

    feat(dsa): solve leetcode 328 odd even linked list

---
+++ 328. Odd Even Linked List/beats.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+ if(!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+            while(even && even->next){
+                odd->next = even->next;
+                odd = odd->next;
+
+                even->next = odd->next;
+                even = even->next;
+            }
+
+            odd->next = evenHead;
+            return head;
+
+
+    }
+};

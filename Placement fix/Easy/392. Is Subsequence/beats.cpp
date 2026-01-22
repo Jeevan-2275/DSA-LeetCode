1eb9a2cf4383713d7cc442ca75aec6524e6a5a03
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-01-22 12:19:52 +0530

    chore: add placement preparation notes and fixes

---
+++ Placement fix/Easy/392. Is Subsequence/beats.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int  i = 0;
+        int count =0;
+        for(int j=0;j<t.size();j++){
+            if(s[i] == t[j]){
+                count++;
+                i++;
+            }
+        }
+        return count == s.size();
+    }
+};

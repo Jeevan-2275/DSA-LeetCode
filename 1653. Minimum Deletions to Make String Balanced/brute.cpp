ba12b09533e716847123e6e9c9930ea07c97c9ed
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-02-08 17:04:08 +0530

    feat(dsa): solve leetcode 1653 minimum deletions to make string balanced

---
+++ 1653. Minimum Deletions to Make String Balanced/brute.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bcount = 0 ;
+        int deletions = 0 ;
+
+        for(char c:s){
+            if(c=='b'){
+                bcount++;
+
+
+            }else{
+                deletions = min(deletions+1,bcount);
+            }
+        }
+        return deletions;
+
+    }
+};

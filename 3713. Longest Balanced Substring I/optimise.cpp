359138a567ba08cd68b41837524c26082d90493b
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-02-13 01:45:45 +0530

    feat(dsa): solve leetcode 3713 longest balanced substring I

---
+++ 3713. Longest Balanced Substring I/optimise.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int ans = 1;
+
+        for (int left = 0; left < n; left++) {
+            vector<int> freq(26, 0);
+            int distinct = 0;
+            int maxFreq = 0;
+
+            for (int right = left; right < n; right++) {
+                int idx = s[right] - 'a';
+
+                if (freq[idx] == 0) distinct++;
+                freq[idx]++;
+                maxFreq = max(maxFreq, freq[idx]);
+
+                int len = right - left + 1;
+
+                if (len == distinct * maxFreq) {
+                    ans = max(ans, len);
+                }
+            }
+        }
+
+        return ans;
+    }
+};

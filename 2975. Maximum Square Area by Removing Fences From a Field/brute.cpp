7116927454afd3581987279b3a08dc8441140d11
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-03-25 00:00:44 +0530

    Add brute solution for LeetCode 2975

--- 2975. Maximum Square Area by Removing Fences From a Field/brute.cpp
+++ 2975. Maximum Square Area by Removing Fences From a Field/brute.cpp
@@ -1,3 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+
 class Solution {
 public:
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {

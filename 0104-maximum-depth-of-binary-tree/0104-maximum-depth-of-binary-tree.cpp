82d87bebb9c3992bdfc8e23a2a0e92a7a189fcf3
Jeevan Kadam <jeevankadam2275@gmail.com>
2026-04-26 14:33:58 +0530

    Time: 0 ms (100%), Space: 18.9 MB (96.69%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+    if(!root){
+        return 0;
+    }
+    return 1 + max(maxDepth(root->left),maxDepth(root->right));
+
+
+
+
+
+
+    }
+};

54964fff091651c110af439b36af807ccc869754
Jeevan Kadam <jeevankadam2275@gmail.com>
2026-08-18 13:53:09 +0530

    Time: 749 ms (5.07%), Space: 69.4 MB (95.55%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+
+        int  ans = 0;
+        for(int i=1;i<nums.size();i++){
+            while(nums[i-1] >= nums[i]){
+                nums[i]++;
+                ans++;
+            }
+
+        }
+        return ans;
+    }
+};

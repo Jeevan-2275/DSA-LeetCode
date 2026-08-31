282366298fc1f3ac654584e8c5de806becfc9638
Jeevan Kadam <jeevankadam2275@gmail.com>
2026-08-31 15:01:35 +0530

    Time: 0 ms (100%), Space: 32.2 MB (42.42%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,58 @@
+// class Solution {
+// public:
+//      bool helper(ListNode* head, TreeNode* root){
+//         if(head == NULL){
+//             return true;
+//         }
+
+//         if(root == NULL ){
+//             return false;
+//         }
+
+//         if(head->val != root->val){
+//             return false;
+//         }
+         
+//         return helper(head->next,root->left) || helper (head->next,root->right);
+//      }
+//     bool isSubPath(ListNode* head, TreeNode* root) {
+//         if(root == NULL){
+//             return false;
+//         }
+//         // return helper(head, root) ||
+//         // isSubPath(head->next,root->left) || isSubPath (head->next,root->right)
+//         // ;
+//         return helper(head->next, root->left) ||
+//        helper(head->next, root->right);
+//     }
+//  };
+// //   return check(head->next, root->left) ||
+// //                check(head->next, root->right);
+
+
+
+class Solution {
+public:
+    bool helper(ListNode* head, TreeNode* root) {
+        if (head == NULL)
+            return true;
+
+        if (root == NULL)
+            return false;
+
+        if (head->val != root->val)
+            return false;
+
+        return helper(head->next, root->left) ||
+               helper(head->next, root->right);
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (root == NULL)
+            return false;
+
+        return helper(head, root) ||
+               isSubPath(head, root->left) ||
+               isSubPath(head, root->right);
+    }
+};

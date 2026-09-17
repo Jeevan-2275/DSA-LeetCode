8419227667262bc55c30620e1d2c3795616c5b06
Jeevan Kadam <jeevankadam2275@gmail.com>
2026-09-17 13:54:07 +0530

    Time: 0 ms (100%), Space: 11.1 MB (65.47%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        
+        std::vector<int> count(1001,0);
+        for(int num : arr1){
+            count[num]++;
+        }
+        std::vector<int> result;
+        result.reserve(arr1.size());
+
+        for(int num :arr2){
+            while(count[num]>0){
+                result.push_back(num);
+                count[num]--;
+            }
+        }
+
+        for(int num =0;num<=1000;++num){
+              while(count[num]>0){
+                result.push_back(num);
+                count[num]--;
+            }
+        }
+        return result;
+    }
+};
